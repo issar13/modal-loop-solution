ab735919649927d7618df51120e4db7efc077bce
issar13 <issa_suleiman008@hotmail.com>
2022-04-03 16:55:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-$('body').on('hidden.bs.modal', '.modal', function () {
-  $(this).find('approvedstat').trigger('reset');
-  $(this).data('.modal', null);
-  $("select").val('0');
-  $("input[name='comment']").prop('disabled', true);
-});
-
-//on modal approved opening ensure  the function ready to be used on the dropdown list
+//this is the jquery function that is used to control modals when it's called there's a similar one that can do the opposite when it's closed
 $('body').on('show.bs.modal', '.modal', function () {
+  //on modal open reset the values inside the form to inital values form name is #approvedstat
   $(this).find('#approvedstat').trigger('reset');
+  //same as above in ensuring the modal has null values
   $(this).data('.modal', null);
+//this is the solution that solved my problem by calling the function inside the modal it ensured the looped modals with same values are isolated and
+//you can basically manipulate the elements individually.
   $("select").change(function(){
     var rejected = $('option:selected',this).text();
     console.log(rejected)
